d33fdf20b939848b8e20ea8d5b756af0e39e3264
Awo2233 <elawo2233@gmail.com>
2025-11-10 16:34:02 +0000

    otro

--- src/api/supabaseClient.js
+++ src/api/supabaseClient.js
@@ -3,8 +3,7 @@ console.log("KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 import { createClient } from '@supabase/supabase-js';
 
-console.log("🟣 VITE_SUPABASE_URL:", import.meta.env.VITE_SUPABASE_URL);
-console.log("🟢 VITE_SUPABASE_ANON_KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
-
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
