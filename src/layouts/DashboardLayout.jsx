785e813146d0b40e4385e973fa17546f7d4dadfb
Awo2233 <elawo2233@gmail.com>
2025-11-24 20:18:14 +0000

    oculando menu

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,16 +1,18 @@
 // src/layouts/DashboardLayout.jsx
 import React from "react";
 import Sidebar from "../components/Sidebar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 import "../styles/Layout.css";
 
 export default function DashboardLayout() {
   const { profile } = useAuth();
+  const location = useLocation();
+  const hideSidebar = location.pathname === "/dashboard";
 
   return (
     <div className="layout-container">
-      <Sidebar role={profile?.role} />
+      {!hideSidebar && <Sidebar role={profile?.role} />}
 
       <main className="layout-content">
         <Outlet />
